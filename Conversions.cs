1544262cb0235c84a13c21d684bca4e6e3f68578
Marq Wright <wright.marques@gmail.com>
2017-12-28 21:36:03 -0500

    Add files via upload

---
+++ Conversions.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate_Conversions
+{
+    public class Conversions
+    {
+        //the conversion variables or fields
+        private string from;
+        private string to;
+        private decimal conNum;
+
+        //empty constructor
+        public Conversions() { }
+
+        //custom constructor
+        public Conversions(string from, string to, decimal conNum)
+        {
+            this.From = from;
+            this.To = to;
+            this.ConNum = conNum;
+        }
+
+        //The From property
+        public string From
+        {
+            get
+            {
+                return from;
+            }
+            set
+            {
+                from = value;
+            }
+        }
+
+        //The To property
+        public string To
+        {
+            get
+            {
+                return to;
+            }
+            set
+            {
+                to = value;
+            }
+        }
+
+        //The Decimal property
+        public decimal ConNum
+        {
+            get
+            {
+                return conNum;
+            }
+            set
+            {
+                conNum = value;
+            }
+        }
+
+        public string GetDisplayText()
+        {
+            return from + " to " + to;
+        }
+
+    }
+}
